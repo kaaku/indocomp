bb0e0e99ca9d0e98b9343193d09c4734263d5902
Markus Yrjölä <markus.yrjola@acelvia.com>
2026-02-16 21:26:36 +0200

    Add grid and main heading

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NgxFudisModule} from '@funidata/ngx-fudis';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxFudisModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
