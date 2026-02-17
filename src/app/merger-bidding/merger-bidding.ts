c694a5b38df86e3761cc36159fa15e899f9f6206
Markus Yrjölä <markus.yrjola@acelvia.com>
2026-02-17 19:27:07 +0200

    Add form model and corresponding inputs to the template

--- src/app/merger-bidding/merger-bidding.ts
+++ src/app/merger-bidding/merger-bidding.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {form, FormField} from "@angular/forms/signals";
 import {NgxFudisModule} from '@funidata/ngx-fudis';
+
+type CompanyType = '' | 'shipping' | 'rice' | 'spice' | 'rice_spice' | 'siap_saji' | 'rubber' | 'oil';
+
+interface BiddingData {
+  companyAGoods: number;
+  companyBGoods: number;
+  companyType: CompanyType;
+}
 
 @Component({
   selector: 'app-merger-bidding',
-  imports: [NgxFudisModule],
+  imports: [NgxFudisModule, FormField],
   templateUrl: './merger-bidding.html',
   styleUrl: './merger-bidding.scss',
 })
-export class MergerBidding {}
+export class MergerBidding {
+
+  protected readonly biddingModel = signal<BiddingData>({
+    companyAGoods: 0,
+    companyBGoods: 0,
+    companyType: '',
+  });
+
+  protected readonly biddingForm = form(this.biddingModel);
+}
